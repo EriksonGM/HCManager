e78dea41c4f56ec53a4e20ab1e0df9d066de5e74
EriksonG <eriksonlove@gmail.com>
2016-06-26 00:17:18 +0100

    Add project files.

---
+++ HCManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HCManager.Startup))]
+namespace HCManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
